a19baec692e4c753e66e343aa29f495cba45b1a0
sarvinoz23 <sarvinozxonmeliqoziyeva4@gmail.com>
2025-03-12 14:35:11 +0500

    tugadi

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-fetch('https://6560acc483aba11d99d151a5.mockapi.io/api/movies')
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-        for (let i = 0; i < data.length; i++){
-            let box = document.querySelector(".box");
-            box.innerHTML +=`       
+fetch("https://6560acc483aba11d99d151a5.mockapi.io/api/movies")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      let box = document.querySelector(".box");
+      box.innerHTML += `       
              <div class="row">
     <div class="col">
       <div class="card">
@@ -13,16 +13,14 @@ fetch('https://6560acc483aba11d99d151a5.mockapi.io/api/movies')
           <span class="card-title">${data[i].movieName}</span>
         </div>
         <div class="card-content">
-          <p>${data[i].starring}....</p>
+          <h6>${data[i].starring}....</h6>
           <p>${data[i].releasedDate}....</p>
         </div>
         <div class="card-action">
-        <a href=${data[i]. trailer} target="_blank">Watch reciept</a>
+        <a href=${data[i].trailer} target="_blank">Watch reciept</a>
         </div>
       </div>
     </div>
-  </div> `
-        }
-    })
-
-
+  </div> `;
+    }
+  });
